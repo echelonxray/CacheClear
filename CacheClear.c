4d589071f97db167a243f5a948dc3c4371c7ab9f
Michael T. Kloos <michael@michaelkloos.com>
2023-06-04 10:40:17 -0400

    Add clear cache code

--- CacheClear.c
+++ CacheClear.c
@@ -6,6 +6,8 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/init.h>
+#include <linux/miscdevice.h>
+#include <linux/fs.h>
 
 MODULE_LICENSE("GPL-2.0");
 MODULE_AUTHOR("Michael T. Kloos");
@@ -13,13 +15,71 @@ MODULE_DESCRIPTION("Adds a simple syscall to run the WBINVD instruction for benc
                    "May not be safe and should only be loaded when need.  ");
 MODULE_VERSION("0.1");
 
+//static int modFileOpen(struct inode *inode, struct file *filp);
+//static int modFileRelease(struct inode *inode, struct file *filp);
+static long modFileIoctl(struct file *filp, unsigned int cmd, unsigned long arg);
+
+static const struct file_operations echelon_fops = {
+	.owner   = THIS_MODULE,
+	//.read             = NULL,
+	//.write            = NULL,
+	//.open             = modFileOpen,
+	//.release          = modFileRelease,
+	//.llseek           = NULL,
+	//.flush            = NULL,
+	.unlocked_ioctl   = modFileIoctl,
+};
+static struct miscdevice echelon_dev = {
+	.minor = MISC_DYNAMIC_MINOR,
+	.name = "ClearCache",
+	.fops = &echelon_fops,
+	//.next = NULL,
+	//.prev = NULL,
+};
+
+/*
+static int modFileOpen(struct inode *inode, struct file *filp) {
+	printk(KERN_INFO "ClearCache: open\n");
+	return 0;
+}
+static int modFileRelease(struct inode *inode, struct file *filp) {
+	printk(KERN_INFO "ClearCache: release\n");
+	return 0;
+}
+*/
+static long modFileIoctl(struct file *filp, unsigned int cmd, unsigned long arg) {
+	printk(KERN_INFO "ClearCache: ioctl(file, %u, %lu)\n", cmd, arg);
+	if (cmd == 1) {
+		printk(KERN_INFO "ClearCache: Clearing Cache\n");
+		__asm__ __volatile__ ("MFENCE \n"
+		                      "LFENCE \n"
+		                      "WBINVD \n");
+	}
+	return 0;
+}
+
 static int __init modInit(void) {
-    printk(KERN_INFO "Loading CacheClear module.\n");
-    return 0;
+	int retval;
+
+	printk(KERN_INFO "Loading CacheClear module...");
+
+	retval = misc_register(&echelon_dev);
+	if (retval) {
+		goto fail_handler;
+	}
+
+	printk(KERN_INFO "Done\n");
+	printk(KERN_INFO "Assigned minor number: %d\n", echelon_dev.minor);
+	return 0;
+
+	fail_handler:
+	printk(KERN_INFO "Failed\n");
+	return 1;
 }
 
 static void __exit modDestroy(void) {
-    printk(KERN_INFO "Unloading CacheClear module.\n");
+	misc_deregister(&echelon_dev);
+	printk(KERN_INFO "Unloaded CacheClear module.\n");
 }
 
 module_init(modInit);
---
+++ module_testing/mod_test.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <errno.h>
+#include <sys/ioctl.h>
+
+int main(int argc, char* argv[]) {
+	int fd;
+	fd = open("/dev/ClearCache", O_RDONLY);
+	if (fd == -1) {
+		printf("Error: Open Failed. errno == %d\n", errno);
+		return 1;
+	}
+
+	int ioctl_retval;
+
+	ioctl_retval = ioctl(fd, 5, 10);
+	printf("ioctl_retval: %d\n", ioctl_retval);
+	ioctl_retval = ioctl(fd, 1, 10);
+	printf("ioctl_retval: %d\n", ioctl_retval);
+
+	close(fd);
+	return 0;
+}
